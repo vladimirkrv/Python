b06deec658df1562461f79ded66c23990d21e4a2
vladimirkrv <144235541+vladimirkrv@users.noreply.github.com>
2023-10-15 19:01:48 +0300

    Task_1.py

--- Лабораторная работа 1/Task_1.py
+++ Лабораторная работа 1/Task_1.py
@@ -1,6 +1,8 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
 # TODO заменить значение пропущенного элемента средним арифметическим
+
 missing_index = 4
-numbers[missing_index] = sum(numbers[:missing_index] + numbers[missing_index +1:]) / len(numbers)
+numbers[missing_index] = sum(numbers[:missing_index] + numbers[missing_index + 1:]) / len(numbers)
+
 print("Измененный список:", numbers)
