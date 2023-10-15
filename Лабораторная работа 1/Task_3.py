d868d700f7668e908aa8c7681eb1c8f14e754e20
vladimirkrv <144235541+vladimirkrv@users.noreply.github.com>
2023-10-15 18:44:24 +0300

    Add files via upload

---
+++ Лабораторная работа 1/Task_3.py
@@ -0,0 +1,10 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+# TODO Разделите участников на две команды
+
+count_players = len(list_players) # TODO кол-во игроков
+count_team = count_players // 2 # TODO число участников одной команды
+team1 = list_players[:count_team] # TODO имя с индексом 0 и имена с четными индексами
+team2 = list_players[count_team:] # TODO имена с нечетными индексами
+print(team1)
+print(team2)
